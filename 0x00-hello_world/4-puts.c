a8f50bdbb1fb848b531fcedfe4f95cb5d56499c1
tvallent <tfvallent@gmail.com>
2022-10-17 12:05:50 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
 /*
- * printing using put
+ * printing using puts
  *
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts( "\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
