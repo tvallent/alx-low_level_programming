fcdc42ee9d714f537b0b51db9bdb0694de9179fd
tvallent <tfvallent@gmail.com>
2022-10-17 13:42:19 -0700

    programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/**main- printing using printf */
+/** main- printing using printf
+ * this isa comment */
 
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n");
