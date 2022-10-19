ce89f73b25a09e6f63e1bf399614ceaa8f5307d4
tvallent <tfvallent@gmail.com>
2022-10-19 05:36:32 -0700

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char c;
 
-
+	c = 'a';
+	while 
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	putchar("\n");
+	return (0);
+}
