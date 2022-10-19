a5c5f94396a5640cbd4c67b9102cad7bc46c38d7
tvallent <tfvallent@gmail.com>
2022-10-19 16:33:47 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * more headers goes there 
+ * main -still going on
+ * betty style doc for function main goes there 
+ */
+int main(void)                                                      
+{	char c;
+	c = 'a';
+	while (c <= 'z')
+	{	if((c !=m'e'||c !='q') && c <= 'z')
+			putchar(c);
+		c++;}
+	putchar('\n');
+	return (0);
+}
