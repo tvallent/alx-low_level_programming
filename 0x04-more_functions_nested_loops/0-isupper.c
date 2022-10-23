fdd6cd540bee762af3fc26c604cae34b7027d565
tvallent <tfvallent@gmail.com>
2022-10-23 05:42:59 -0700

    Task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
 /**
  * _isupper -uppercase letter
  * @c: char to check
  *
  * Return: 0 or 1
  */
-
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
