113fe89386f85a66b4e252a3c809979be71e39c2
tvallent <tfvallent@gmail.com>
2022-10-23 16:53:05 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success/correct)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
 
 int main(void)
 {
