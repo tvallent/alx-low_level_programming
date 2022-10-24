adacfa6419dbbf4535dc07dbb285a14cf62a4eb2
tvallent <tfvallent@gmail.com>
2022-10-24 03:10:31 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * swap_int - swaps the values of the integers using two input parameters
+ *
+ * @a: parameter1
+ * @b: parameter2
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
