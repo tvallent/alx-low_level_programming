853b60832f65969e3c283ae9b657428d72e19dba
tvallent <tfvallent@gmail.com>
2022-10-23 18:08:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <studio.h>
-/*
- * main - printing using puts.
- *
+#include <stdio.h>
+
+/**
+ * main - print out a string stdout.
  * Return: 0 if success.
  */
+
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
