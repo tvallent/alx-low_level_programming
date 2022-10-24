143493caf6213b3f40a125546697059d9e81b01b
tvallent <tfvallent@gmail.com>
2022-10-24 04:13:29 -0700

    2-strle n.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string pointer to an int that will be updated
+ *
+ * Return: returns length of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
