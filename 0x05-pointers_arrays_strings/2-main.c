32ca3cf88cf8c5a42fc18bdccd9cb42407bca7d9
tvallent <tfvallent@gmail.com>
2022-10-24 04:25:57 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	 char *str;
 	 int len;
+
 	 str = "My first strlen!";
 	 len = _strlen(str);
 	 printf("%d\n", len);
+	 
 	 return (0);
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,9 +10,11 @@
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
+
 	return (len);
 }
