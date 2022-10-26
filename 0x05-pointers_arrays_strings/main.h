dfe01fadc600dde8a53e9e383708307c74d815a0
tvallent <tfvallent@gmail.com>
2022-10-26 04:04:33 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,17 +5,9 @@
  * void prototypes()
  * int prototypes()
  */
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
+void _putchar(char c );
 void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
 
 #endif /* MAIN_H */
