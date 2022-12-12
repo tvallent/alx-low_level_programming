154251f9c18ba23f106c0eaf2649e0d12f6d2067
tvallent <tfvallent@gmail.com>
2022-12-11 22:55:59 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,50 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _calloc - function that allocates memory for an array, using malloc
- * @nmemb: size of the memory space to allocate in bytes
- * @size: size of type
- * Return: void pointer
- */
-
+  * _calloc - ...
+  * @nmemb: number of members
+  * @size: size
+  *
+  * Return: ...
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	int i = 0, l = 0;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-	{
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
 		return (NULL);
-	}
-	_memset(ptr, 0, size * nmemb);
-	return (ptr);
-}
 
-/**
- * _memset - function that fills memory with a constant byte
- *
- * @s: parameter defined in main, pointer to memory area
- * @b: parameter defined in main, constant byte
- * @n: parameter defined in main, number of bytes to be filled
- *
- * Return: memory address of function (memory area)
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-	char *tmp = s;
-
-	for (i = 0; i < n; i++)
+	while (i < l)
 	{
-		*s = b;
-		s++;
+		p[i] = 0;
+		i++;
 	}
-	s = tmp;
-	return (s);
+
+	return (p);
 }
